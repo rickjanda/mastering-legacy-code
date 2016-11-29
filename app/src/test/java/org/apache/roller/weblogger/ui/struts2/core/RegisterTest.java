f31800a9ecfe171f3894b0890569e7d54bfec43a
rja <rick.janda@zuehlke.com>
2016-11-29 13:31:09 +0100

    test improvement to catch missing activationcode

--- app/src/test/java/org/apache/roller/weblogger/ui/struts2/core/RegisterTest.java
+++ app/src/test/java/org/apache/roller/weblogger/ui/struts2/core/RegisterTest.java
@@ -19,39 +19,42 @@ public class RegisterTest {
 
     @Test
     public void save_successful() throws Exception {
-        ProfileBean profileBean1 = new ProfileBean();
-        profileBean1.setUserName("Peterchen");
-        profileBean1.setPasswordText("This is an amazing text.");
-        profileBean1.setOpenIdUrl("http://peterchenopenidurl.ch");
-        profileBean1.setPasswordConfirm(profileBean1.getPasswordText());
-        ProfileBean profileBean = profileBean1;
+        ProfileBean profileBean = new ProfileBean();
+        profileBean.setUserName("Peterchen");
+        profileBean.setPasswordText("This is an amazing text.");
+        profileBean.setOpenIdUrl("http://peterchenopenidurl.ch");
+        profileBean.setPasswordConfirm(profileBean.getPasswordText());
         ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
 
         HttpServletRequest httpServletRequest = mock(HttpServletRequest.class, RETURNS_DEEP_STUBS);
         WebloggerRuntimeConfigInstance webloggerRuntimeConfigInstance = mock(WebloggerRuntimeConfigInstance.class);
-        Register register = spy(new Register(mock(MailUtilInstance.class), webloggerRuntimeConfigInstance));
-        Weblogger weblogger = mock(Weblogger.class, RETURNS_DEEP_STUBS);
+        Register registerSpy = spy(new Register(mock(MailUtilInstance.class), webloggerRuntimeConfigInstance));
+        Weblogger webloggerMock = mock(Weblogger.class, RETURNS_DEEP_STUBS);
 
-        doReturn(weblogger).when(register).getWeblogger();
+        doReturn(webloggerMock).when(registerSpy).getWeblogger();
         doReturn(true).when(webloggerRuntimeConfigInstance).getBooleanProperty("users.registration.enabled");
         doReturn(true).when(webloggerRuntimeConfigInstance).getBooleanProperty("user.account.email.activation");
-        when(weblogger.getUserManager().getUserByUserName("Peterchen", null)).thenReturn(null);
-        when(weblogger.getUserManager().getUserByOpenIdUrl("http://peterchenopenidurl.ch")).thenReturn(null);
-        when(weblogger.getUserManager().getUserByActivationCode(anyString())).thenReturn(null);
 
-        register.setServletRequest(httpServletRequest);
-        register.setBean(profileBean);
+        UserManager userManagerMock = webloggerMock.getUserManager();
+        when(userManagerMock.getUserByUserName(anyString(), (Boolean) isNull())).thenReturn(null);
+        when(userManagerMock.getUserByOpenIdUrl(anyString())).thenReturn(null);
+        when(userManagerMock.getUserByActivationCode(anyString())).thenReturn(null);
+
+        registerSpy.setServletRequest(httpServletRequest);
+        registerSpy.setBean(profileBean);
 
         // act
-        String saveResult = register.save();
+        String saveResult = registerSpy.save();
 
-        verify(weblogger.getUserManager()).addUser(userArgumentCaptor.capture());
+        verify(userManagerMock).addUser(userArgumentCaptor.capture());
         User savedUser = userArgumentCaptor.getValue();
         assertEquals(Action.SUCCESS, saveResult);
         assertEquals("ProfileBean{id='null', userName='Peterchen', password='null', screenName='null', fullName='null', emailAddress='null', locale='null', timeZone='null', openIdUrl='http://peterchenopenidurl.ch', passwordText='This is an amazing text.', passwordConfirm='This is an amazing text.'}", profileBean.getStateAsString());
         assertEquals("User{, userName='Peterchen', password='This is an amazing text.', openIdUrl='http://peterchenopenidurl.ch', screenName='null', fullName='null', emailAddress='null', enabled=false}", savedUser.getStateAsString());
         assertTrue(StringUtils.isNotEmpty(savedUser.getId()));
         assertNotNull(savedUser.getDateCreated());
+        assertNotNull(savedUser.getActivationCode());
+
     }
 
 }
